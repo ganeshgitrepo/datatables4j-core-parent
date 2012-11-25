2f1382e214d4ced2dfdea3943bffa4c97148e36b
Romain Lespinasse <romain.lespinasse@gmail.com>
2012-11-25 22:35:41 +0100

    fix JSON generation when a String array is used instead of List
    JSON-simple don't manage a String array and convert it as [Ljava.lang.String;@550c6b37
    
    relative to issue #4

--- datatables4j-core-impl/src/main/java/com/github/datatables4j/core/generator/ConfigGenerator.java
+++ datatables4j-core-impl/src/main/java/com/github/datatables4j/core/generator/ConfigGenerator.java
@@ -17,18 +17,14 @@
  */
 package com.github.datatables4j.core.generator;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import com.github.datatables4j.core.api.constants.DTConstants;
+import com.github.datatables4j.core.api.model.HtmlColumn;
+import com.github.datatables4j.core.api.model.HtmlTable;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.github.datatables4j.core.api.constants.DTConstants;
-import com.github.datatables4j.core.api.model.HtmlColumn;
-import com.github.datatables4j.core.api.model.HtmlTable;
+import java.util.*;
 
 /**
  * Class in charge of DataTables configuration generation.
@@ -37,111 +33,112 @@ import com.github.datatables4j.core.api.model.HtmlTable;
  */
 public class ConfigGenerator {
 
-	// Logger
-	private static Logger logger = LoggerFactory.getLogger(ConfigGenerator.class);
-	
-	/**
-	 * If no custom config is specified with table attributes, DataTables
-	 * will internally use default one.
-	 * 
-	 * @param table
-	 *            The POJO containing the HTML table.
-	 * @return MainConf The main configuration file associated with the HTML
-	 *         table.
-	 */
-	public Map<String, Object> generateConfig(HtmlTable table) {
+    // Logger
+    private static Logger logger = LoggerFactory.getLogger(ConfigGenerator.class);
 
-		logger.debug("Generating DataTables configuration ..");
-		
-		// Main configuration object
-		Map<String, Object> mainConf = new HashMap<String, Object>();
-		
-		// Columns configuration
-		Map<String, Object> tmp = null;
-		List<Map<String, Object>> aoColumnsContent = new ArrayList<Map<String, Object>>(); 
-		for (HtmlColumn column : table.getLastHeaderRow().getColumns()) {
-			tmp = new HashMap<String, Object>();
-			
-			// Sortable
-			tmp.put(DTConstants.DT_SORTABLE, column.getSortable());
-			
-			//
-			if(column.getProperty() != null){
-				tmp.put(DTConstants.DT_DATA, column.getProperty());				
-			}
+    /**
+     * If no custom config is specified with table attributes, DataTables
+     * will internally use default one.
+     *
+     * @param table The POJO containing the HTML table.
+     * @return MainConf The main configuration file associated with the HTML
+     *         table.
+     */
+    public Map<String, Object> generateConfig(HtmlTable table) {
 
-			// Sorting direction
-			if(StringUtils.isNotBlank(column.getSortDirection())){
-				tmp.put(DTConstants.DT_SORT_DIR, column.getSortDirection().trim().toLowerCase().split(","));
-			}
-			
-			aoColumnsContent.add(tmp);
-		}
-		mainConf.put(DTConstants.DT_AOCOLUMNS, aoColumnsContent);
-		
-		// Column sorting initialisation
-		List<Object> aaSortingtmp = null;
-		List<Object> aaSortingContent = new ArrayList<Object>();
-		Integer columnIndex = 0;
-		for (HtmlColumn column : table.getLastHeaderRow().getColumns()) {
-			
-			// Sorting direction
-			if(StringUtils.isNotBlank(column.getSortInit())){
-				aaSortingtmp = new ArrayList<Object>();
-				aaSortingtmp.add(columnIndex);
-				aaSortingtmp.add(column.getSortInit());
-				aaSortingContent.add(aaSortingtmp);
-			}
-			
-			columnIndex++;
-		}
-		if(!aaSortingContent.isEmpty()){
-			mainConf.put(DTConstants.DT_SORT_INIT, aaSortingContent);			
-		}
-		
-		if(table.getLabels() != null){
-			tmp = new HashMap<String, Object>();
-			tmp.put(DTConstants.DT_URL, table.getLabels());
-			mainConf.put(DTConstants.DT_LANGUAGE, tmp);
-		}
-		if (table.getAutoWidth() != null) {
-			mainConf.put(DTConstants.DT_AUTO_WIDTH, table.getAutoWidth());
-		}
-		if (table.getDeferRender() != null) {
-			mainConf.put(DTConstants.DT_DEFER_RENDER, table.getDeferRender());
-		}
-		if (table.getFilterable() != null) {
-			mainConf.put(DTConstants.DT_FILTER, table.getFilterable());
-		}
-		if (table.getInfo() != null) {
-			mainConf.put(DTConstants.DT_INFO, table.getInfo());
-		}
-		if (table.getPaginate() != null) {
-			mainConf.put(DTConstants.DT_PAGINATE, table.getPaginate());
-		}
-		if (table.getLengthChange() != null) {
-			mainConf.put(DTConstants.DT_LENGTH_CHANGE, table.getLengthChange());
-		}
-		if (StringUtils.isNotBlank(table.getPaginationStyle())) {
-			mainConf.put(DTConstants.DT_PAGINATION_TYPE, table.getPaginationStyle());
-		}
-		if (table.getProcessing() != null) {
-			mainConf.put(DTConstants.DT_PROCESSING, table.getProcessing());
-		}
-		if (table.getSort() != null) {
-			mainConf.put(DTConstants.DT_SORT, table.getSort());
-		}
-		if (table.getStateSave() != null) {
-			mainConf.put(DTConstants.DT_STATE_SAVE, table.getStateSave());
-		}
-		if (table.getJqueryUI() != null) {
-			mainConf.put(DTConstants.DT_JQUERYUI, table.getJqueryUI());
-		}
-		
-		mainConf.put(DTConstants.DT_DOM, "lfrtip");
-		
-		logger.debug("DataTables configuration generated");
-		
-		return mainConf;
-	}
+        logger.debug("Generating DataTables configuration ..");
+
+        // Main configuration object
+        Map<String, Object> mainConf = new HashMap<String, Object>();
+
+        // Columns configuration
+        Map<String, Object> tmp = null;
+        List<Map<String, Object>> aoColumnsContent = new ArrayList<Map<String, Object>>();
+        for (HtmlColumn column : table.getLastHeaderRow().getColumns()) {
+            tmp = new HashMap<String, Object>();
+
+            // Sortable
+            tmp.put(DTConstants.DT_SORTABLE, column.getSortable());
+
+            //
+            if (column.getProperty() != null) {
+                tmp.put(DTConstants.DT_DATA, column.getProperty());
+            }
+
+            // Sorting direction
+            if (StringUtils.isNotBlank(column.getSortDirection())) {
+                List<Object> sortDirection = new ArrayList<Object>();
+                Collections.addAll(sortDirection, column.getSortDirection().trim().toLowerCase().split(","));
+                tmp.put(DTConstants.DT_SORT_DIR, sortDirection);
+            }
+
+            aoColumnsContent.add(tmp);
+        }
+        mainConf.put(DTConstants.DT_AOCOLUMNS, aoColumnsContent);
+
+        // Column sorting initialisation
+        List<Object> aaSortingtmp = null;
+        List<Object> aaSortingContent = new ArrayList<Object>();
+        Integer columnIndex = 0;
+        for (HtmlColumn column : table.getLastHeaderRow().getColumns()) {
+
+            // Sorting direction
+            if (StringUtils.isNotBlank(column.getSortInit())) {
+                aaSortingtmp = new ArrayList<Object>();
+                aaSortingtmp.add(columnIndex);
+                aaSortingtmp.add(column.getSortInit());
+                aaSortingContent.add(aaSortingtmp);
+            }
+
+            columnIndex++;
+        }
+        if (!aaSortingContent.isEmpty()) {
+            mainConf.put(DTConstants.DT_SORT_INIT, aaSortingContent);
+        }
+
+        if (table.getLabels() != null) {
+            tmp = new HashMap<String, Object>();
+            tmp.put(DTConstants.DT_URL, table.getLabels());
+            mainConf.put(DTConstants.DT_LANGUAGE, tmp);
+        }
+        if (table.getAutoWidth() != null) {
+            mainConf.put(DTConstants.DT_AUTO_WIDTH, table.getAutoWidth());
+        }
+        if (table.getDeferRender() != null) {
+            mainConf.put(DTConstants.DT_DEFER_RENDER, table.getDeferRender());
+        }
+        if (table.getFilterable() != null) {
+            mainConf.put(DTConstants.DT_FILTER, table.getFilterable());
+        }
+        if (table.getInfo() != null) {
+            mainConf.put(DTConstants.DT_INFO, table.getInfo());
+        }
+        if (table.getPaginate() != null) {
+            mainConf.put(DTConstants.DT_PAGINATE, table.getPaginate());
+        }
+        if (table.getLengthChange() != null) {
+            mainConf.put(DTConstants.DT_LENGTH_CHANGE, table.getLengthChange());
+        }
+        if (StringUtils.isNotBlank(table.getPaginationStyle())) {
+            mainConf.put(DTConstants.DT_PAGINATION_TYPE, table.getPaginationStyle());
+        }
+        if (table.getProcessing() != null) {
+            mainConf.put(DTConstants.DT_PROCESSING, table.getProcessing());
+        }
+        if (table.getSort() != null) {
+            mainConf.put(DTConstants.DT_SORT, table.getSort());
+        }
+        if (table.getStateSave() != null) {
+            mainConf.put(DTConstants.DT_STATE_SAVE, table.getStateSave());
+        }
+        if (table.getJqueryUI() != null) {
+            mainConf.put(DTConstants.DT_JQUERYUI, table.getJqueryUI());
+        }
+
+        mainConf.put(DTConstants.DT_DOM, "lfrtip");
+
+        logger.debug("DataTables configuration generated");
+
+        return mainConf;
+    }
 }
