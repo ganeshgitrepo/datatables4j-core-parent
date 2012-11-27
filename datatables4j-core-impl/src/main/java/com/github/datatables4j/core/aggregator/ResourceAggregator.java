3aab47adb69e7159ec839d6496c70afa4c20988b
Thibault Duchateau <thibault.duchateau@gmail.com>
2012-11-27 08:42:30 +0100

    New BSD licence

--- datatables4j-core-impl/src/main/java/com/github/datatables4j/core/aggregator/ResourceAggregator.java
+++ datatables4j-core-impl/src/main/java/com/github/datatables4j/core/aggregator/ResourceAggregator.java
@@ -1,19 +1,31 @@
 /*
- * DataTables4j, a JSP taglib to display table with jQuery and DataTables
- * Copyright (c) 2012, DataTables4j <datatables4j@gmail.com>
- *
- * This Program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2, as
- * published by the Free Software Foundation.
+ * [The "BSD licence"]
+ * Copyright (c) 2012 DataTables4j
+ * All rights reserved.
  * 
- * The Program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  * 
- * You should have received a copy of the GNU General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of DataTables4j nor the names of its contributors 
+ * may be used to endorse or promote products derived from this software 
+ * without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package com.github.datatables4j.core.aggregator;
 
@@ -32,7 +44,7 @@ import com.github.datatables4j.core.util.ResourceHelper;
 
 /**
  * Web resources aggregator.
- *
+ * 
  * @author Thibault Duchateau
  */
 public class ResourceAggregator {
@@ -50,9 +62,10 @@ public class ResourceAggregator {
 	 *            The table containing the datatatables4j configuration.
 	 */
 	public static void processAggregation(WebResources webResources, HtmlTable table) {
-		
-		logger.debug("Processing aggregation, using configuration {}", table.getTableProperties().getAggregatorMode());
-		
+
+		logger.debug("Processing aggregation, using configuration {}", table.getTableProperties()
+				.getAggregatorMode());
+
 		switch (table.getTableProperties().getAggregatorMode()) {
 		case ALL:
 			aggregateAll(webResources);
@@ -65,12 +78,12 @@ public class ResourceAggregator {
 		case PLUGINS_CSS:
 			aggregatePluginsCss(webResources);
 			break;
-			
+
 		default:
 			break;
 		}
 	}
-	
+
 	/**
 	 * All web resources are aggregated. <li>All javascript resources will be
 	 * merge into one file <li>All stylesheets resources will be merge into one
@@ -79,101 +92,108 @@ public class ResourceAggregator {
 	 * @param webResources
 	 *            The wrapper POJO containing all web resources to aggregate.
 	 */
-	public static void aggregateAll(WebResources webResources){
-		
-		String jsResourceName = NameConstants.DT_AGG_ALL_JS + ResourceHelper.getRamdomNumber() + ".js";
-		String cssResourceName = NameConstants.DT_AGG_ALL_CSS + ResourceHelper.getRamdomNumber() + ".css";
-		
+	public static void aggregateAll(WebResources webResources) {
+
+		String jsResourceName = NameConstants.DT_AGG_ALL_JS + ResourceHelper.getRamdomNumber()
+				+ ".js";
+		String cssResourceName = NameConstants.DT_AGG_ALL_CSS + ResourceHelper.getRamdomNumber()
+				+ ".css";
+
 		JsResource aggregateJsFile = new JsResource(ResourceType.AGGREGATE, jsResourceName);
 		CssResource aggregateCssFile = new CssResource(cssResourceName);
-		
+
 		String aggregatedJsContent = "";
 		String aggregatedCssContent = "";
-		
+
 		for (Entry<String, JsResource> entry : webResources.getJavascripts().entrySet()) {
 			aggregatedJsContent += entry.getValue().getContent();
 		}
 		aggregateJsFile.setContent(aggregatedJsContent);
-		
+
 		for (Entry<String, CssResource> entry : webResources.getStylesheets().entrySet()) {
 			aggregatedCssContent += entry.getValue().getContent();
 		}
 		aggregateCssFile.setContent(aggregatedCssContent);
-		
+
 		// All existing Javascript resources are removed
 		webResources.getJavascripts().clear();
-		
+
 		// Add aggregated Javascript resource
 		webResources.getJavascripts().put(aggregateJsFile.getName(), aggregateJsFile);
-		
+
 		// All existing Stylesheets resources are removed
 		webResources.getStylesheets().clear();
-		
+
 		// Add aggregated stylesheet resource
 		webResources.getStylesheets().put(aggregateCssFile.getName(), aggregateCssFile);
-		
+
 		logger.debug("Aggregation (ALL) completed");
 	}
-	
+
 	/**
-	 * Only javascript resources are aggregated. The other ones remain unchanged.
+	 * Only javascript resources are aggregated. The other ones remain
+	 * unchanged.
 	 * 
 	 * @param webResources
 	 *            The wrapper POJO containing all web resources to aggregate.
 	 */
-	public static void aggregatePluginsJs(WebResources webResources){
-		
-		String jsResourceName = NameConstants.DT_AGG_PLUGINS_JS + ResourceHelper.getRamdomNumber() + ".js";
+	public static void aggregatePluginsJs(WebResources webResources) {
+
+		String jsResourceName = NameConstants.DT_AGG_PLUGINS_JS + ResourceHelper.getRamdomNumber()
+				+ ".js";
 		JsResource aggregatePluginsJsFile = new JsResource(jsResourceName);
-		
+
 		String aggregatedJsContent = "";
-		
+
 		for (Entry<String, JsResource> entry : webResources.getJavascripts().entrySet()) {
-			
+
 			// Filter plugin only
-			if(entry.getValue().getType().equals(ResourceType.PLUGIN)){
+			if (entry.getValue().getType().equals(ResourceType.PLUGIN)) {
 				aggregatedJsContent += entry.getValue().getContent();
 			}
-			
+
 			// Remove the plugin JsResource from the map
-			webResources.getJavascripts().remove(entry.getKey());			
+			webResources.getJavascripts().remove(entry.getKey());
 		}
-				
+
 		aggregatePluginsJsFile.setContent(aggregatedJsContent);
-		
+
 		webResources.getJavascripts().put(aggregatePluginsJsFile.getName(), aggregatePluginsJsFile);
-		
+
 		logger.debug("Aggregation (PLUGINS_JS) completed");
 	}
-	
+
 	/**
-	 * Only stylesheet resources are aggregated. The other ones remain unchanged.
+	 * Only stylesheet resources are aggregated. The other ones remain
+	 * unchanged.
 	 * 
 	 * @param webResources
 	 *            The wrapper POJO containing all web resources to aggregate.
 	 */
-	public static void aggregatePluginsCss(WebResources webResources){
-		
-		String cssResourceName = NameConstants.DT_AGG_PLUGINS_CSS + ResourceHelper.getRamdomNumber() + ".css";
+	public static void aggregatePluginsCss(WebResources webResources) {
+
+		String cssResourceName = NameConstants.DT_AGG_PLUGINS_CSS
+				+ ResourceHelper.getRamdomNumber() + ".css";
 		CssResource aggregatePluginsCssFile = new CssResource(cssResourceName);
-		
+
 		String aggregatedCssContent = "";
-		
+
 		for (Entry<String, CssResource> entry : webResources.getStylesheets().entrySet()) {
-			
+
 			// Filter plugin only
-			if(entry.getValue().getType().equals(ResourceType.PLUGIN)){
+			if (entry.getValue().getType().equals(ResourceType.PLUGIN)) {
 				aggregatedCssContent += entry.getValue().getContent();
 			}
-			
+
 			// Remove the plugin JsResource from the map
-			webResources.getStylesheets().remove(entry.getKey());			
+			webResources.getStylesheets().remove(entry.getKey());
 		}
-				
+
 		aggregatePluginsCssFile.setContent(aggregatedCssContent);
-		
-		webResources.getStylesheets().put(aggregatePluginsCssFile.getName(), aggregatePluginsCssFile);
-		
+
+		webResources.getStylesheets().put(aggregatePluginsCssFile.getName(),
+				aggregatePluginsCssFile);
+
 		logger.debug("Aggregation (PLUGINS_CSS) completed");
 	}
 }
