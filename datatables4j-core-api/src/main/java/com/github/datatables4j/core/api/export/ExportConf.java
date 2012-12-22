8966579956436b705ce5f5cb7a5170f30327369c
Thibault Duchateau <thibault.duchateau@gmail.com>
2012-12-22 19:56:37 +0100

    Refactoring and comments

---
+++ datatables4j-core-api/src/main/java/com/github/datatables4j/core/api/export/ExportConf.java
@@ -0,0 +1,138 @@
+/*
+ * [The "BSD licence"]
+ * Copyright (c) 2012 DataTables4j
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of DataTables4j nor the names of its contributors 
+ * may be used to endorse or promote products derived from this software 
+ * without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.github.datatables4j.core.api.export;
+
+
+/**
+ * POJO that stores an export type configuration.
+ * 
+ * @author Thibault Duchateau
+ */
+public class ExportConf {
+
+	private String fileName;
+	private String type;
+	private String label;
+	private StringBuffer cssStyle;
+	private StringBuffer cssClass;
+	private ExportLinkPosition position;
+	private Boolean includeHeader;
+	private String area;
+	private String url;
+	private Boolean autoSize;
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public StringBuffer getCssStyle() {
+		return cssStyle;
+	}
+
+	public void setCssStyle(StringBuffer cssStyle) {
+		this.cssStyle = cssStyle;
+	}
+
+	public StringBuffer getCssClass() {
+		return cssClass;
+	}
+
+	public void setCssClass(StringBuffer cssClass) {
+		this.cssClass = cssClass;
+	}
+
+	public ExportLinkPosition getPosition() {
+		return position;
+	}
+
+	public void setPosition(ExportLinkPosition position) {
+		this.position = position;
+	}
+
+	public Boolean getIncludeHeader() {
+		return includeHeader;
+	}
+
+	public void setIncludeHeader(Boolean includeHeader) {
+		this.includeHeader = includeHeader;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public String toString() {
+		return "ExportConf [fileName=" + fileName + ", type=" + type + ", label=" + label
+				+ ", cssStyle=" + cssStyle + ", cssClass=" + cssClass + ", position=" + position
+				+ ", includeHeader=" + includeHeader + ", area=" + area + ", url=" + url + "]";
+	}
+
+	public Boolean getAutoSize() {
+		return autoSize;
+	}
+
+	public void setAutoSize(Boolean autoSize) {
+		this.autoSize = autoSize;
+	}
+
+}
