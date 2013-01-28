4caeb6efdd124e5b43b534b52715ff2b7a6fce62
Thibault Duchateau <thibault.duchateau@gmail.com>
2013-01-28 01:15:38 +0100

    AJAX constants added
    datatables4j/issues#12

--- datatables4j-core-api/src/main/java/com/github/datatables4j/core/api/constants/DTConstants.java
+++ datatables4j-core-api/src/main/java/com/github/datatables4j/core/api/constants/DTConstants.java
@@ -45,8 +45,28 @@ public class DTConstants {
 	public static final String DT_SORT = "bSort";
 	public static final String DT_PAGINATE = "bPaginate";
 	public static final String DT_LENGTH_CHANGE = "bLengthChange";
-	public static final String DT_PROCESSING = "bProcessing";
 	public static final String DT_STATE_SAVE = "bStateSave";
+
+	// AJAX related constants
+	public static final String DT_PROCESSING = "bProcessing";
+	public static final String DT_SERVER_SIDE = "bServerSide";
+	public static final String DT_AJAX_SOURCE = "sAjaxSource";
+	
+	// AJAX URL parameters
+	public static final String DT_S_ECHO = "sEcho";
+	public static final String DT_I_COLUMNS = "iColumns";
+	public static final String DT_S_COLUMNS = "sColumns";
+	public static final String DT_I_DISPLAY_START = "iDisplayStart";
+	public static final String DT_I_DISPLAY_LENGTH = "iDisplayLength";
+	public static final String DT_M_DATA_PROP= "mDataProp_";
+	public static final String DT_S_SEARCH= "sSearch";
+	public static final String DT_B_REGEX= "bRegex";
+	public static final String DT_B_SEARCHABLE= "bSearchable_";
+	public static final String DT_B_SORTABLE= "bSortable_";
+	public static final String DT_I_SORT_COL= "iSortCol_";
+	public static final String DT_S_SORT_DIR= "sSortDir_";
+	public static final String DT_I_SORTING_COLS = "iSortingCols";
+	public static final String DT_B_REGEX_COL = "bRegex_";
 	
 	// Advanced configuration
 	public static final String DT_PAGINATION_TYPE = "sPaginationType";
